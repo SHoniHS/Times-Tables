f6b1d8dcfd5cf72fddede01bbef6e0ee6602c465
SHS <SHS@macbook-pro-de-shs.local>
2018-12-02 08:23:21 +0100

    Initial Commit

---
+++ Times Tables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Times Tables
+//
+//  Created by SHS on 2/12/18.
+//  Copyright © 2018 SHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
